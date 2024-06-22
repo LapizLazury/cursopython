fa37d18fb55f3064f34ba58a4688746fdfe677f2
LapizLazury <sharonunahur2023@gmail.com>
2024-06-22 01:29:55 -0300

    Subiendo tarea de las clases

---
+++ tareas_clases.py
@@ -0,0 +1,13 @@
+#DESTINADO A LAS TAREAS
+
+
+#CLASE 18/6: CREAR UN SCRIPT EN EL QUE PODAMOS CALCULAR LA NOTA FINAL EN BASE A 5 EXAMENES, ESTOS EXAMENES EQUIVALEN A UN PORCENTAJE DE LA NOTA FINAL.
+
+# LA NOTA NUMERO 1 CORRESPONDE AL 20% DE LA NOTA FINAL.
+# LA NOTA NUMERO 2 CORRESPONDE AL 10% DE LA NOTA FINAL.
+# LA NOTA NUMERO 3 CORRESPONDE AL 10% DE LA NOTA FINAL.
+# LA NOTA NUMERO 4 CORRESPONDE AL 10% DE LA NOTA FINAL.
+# LA NOTA NUMERO 5 CORRESPONDE AL 50% DE LA NOTA FINAL.
+
+#A esto se lo suele llamar promedio ponderado: no todos los valores tienen el mismo "peso".
+#Por ejemplo, dado el ejercicio de arriba me conviene sacarme una mejor nota en el examen donde la nota vale casi el 50% de la nota final.
