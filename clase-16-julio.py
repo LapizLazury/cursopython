3da915756de5574a0778ad48758baeb1717eb3f5
LapizLazury <sharonunahur2023@gmail.com>
2024-07-16 19:10:23 -0300

    clase 16/07/24

--- clase-16-julio.py
+++ clase-16-julio.py
@@ -1,6 +1,38 @@
-edad= int(input("que esdad tenes?"))
-if edad >= 18:
-    print("sos adulto")
+#SENTENCIAS INTERATIVAS
+#WHILE
+#FOR
+
+# EL flujo de ejecucion de una sentencia WHILE:
+#1. Se evalúa la expresión de control.
+#2. Si es verdadera, se ejecuta el cuerpo de la sentencia.
+#3. Se vuelve a evaluar la expresión de control y se repite el paso 2 hasta que se evalúe como falso.
+
+#1. E
+
+numero = 10 #la variable
+while numero >0:
+    print(numero)
+    numero-= 1
+print("termino el conteo")   
+
+n = 0
+while n <=5:
+    n+=1
+    print("N vale", n)
     
-    if False:
-        print("IMPRIME LA CONDICION VERDADERA")
+#WHILE condicion :
+#  intrucciones de while
+#else
+#  intrucciones de while-else
+#si noce termino con break
+#  intrucciones de while-else
+
+chanse =1
+while chanse <= 3:
+    txt = input("Escribe SI: ")
+    if txt == "SI": 
+        print("Ok, lo conseguiste en el intento", chanse)
+        break
+    chanse +1
+else:
+    print("No lograste en los 3 intentos")
